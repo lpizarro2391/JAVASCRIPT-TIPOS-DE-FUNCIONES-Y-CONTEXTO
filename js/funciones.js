84d21b28504fdc6456ecccdd2a1c895988fe8709
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-01-22 11:23:53 -0600

    funciones autoejecutables

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,5 @@
+//Funciones Anonimas
+/*
 setInterval( function (){
   console.log ('Mensaje repetitivo');
 },2000);
@@ -12,12 +14,21 @@ function Personalizada(numero1,numero2,function2){
 
 Personalizada(4,8,function(resultado){ alert(resultado);});
 
+*/
 //Funciones autoejecutables
 
 (function(){
   console.log(this);//Aqui el resultado this es igual a window
 })();
 
+(function(){
+  this.variable1 = "Hola Mundo";
+})();
+
+console.log(window.variable1);
+/*
 (function(nombre,apellido){
   console.log(nombre + '' + apellido);
 })('Andres',Mensaje);
+
+*/
